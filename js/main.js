3c9987360476f1f1b4ac4b76398bbd2ffbbd02bb
Matheus <matheusmendesreis@outlook.com>
2024-02-06 12:14:57 -0300

    desafio 01 oncluido

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const url = "https://desafio.xlow.com.br/search";
+const productContainer = document.querySelector("#products-container");
+
+async function getAllProducts() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const countDiv = document.createElement("div");
+    countDiv.classList.add("product-modal"); 
+    countDiv.innerText = `Total Products: ${data.length}`;
+    productContainer.appendChild(countDiv);
+
+    data.map((prod) => {
+      const card = document.createElement("li");
+      card.classList.add("product-card"); 
+
+      const title = document.createElement("h2");
+      title.innerText = prod.productName;
+
+      const brand = document.createElement("p");
+      brand.innerText = `Marca: ${prod.brand}`;
+
+      const image = document.createElement("img");
+      image.src = prod.image;
+      image.alt = prod.productName;
+
+      const listPrice = document.createElement("p");
+      listPrice.innerText = `Preço de Lista: R$ ${prod.listPrice / 100}`;
+
+      const bestPrice = document.createElement("p");
+      bestPrice.innerText = `Melhor Preço: R$ ${prod.bestPrice / 100}`;
+
+      const button = document.createElement("button");
+      button.innerText = "Ver Produto";
+      button.addEventListener("click", () => {
+          window.open(prod.link, "_blank");
+      });
+      
+      card.appendChild(title);
+      card.appendChild(brand);
+      card.appendChild(image);
+      card.appendChild(listPrice);
+      card.appendChild(bestPrice);
+      card.appendChild(button)
+      productContainer.appendChild(card);
+    });
+  } catch (error) {
+    console.error("Erro ao obter produtos:", error);
+  }
+}
+
+getAllProducts();
