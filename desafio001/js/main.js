3aa7a95e703d190c90e71e9ed02da5fbb1fd4eb3
Matheus <matheusmendesreis@outlook.com>
2024-02-07 16:17:07 -0300

    feat: adiconei interação com as imagens da api

--- desafio001/js/main.js
+++ desafio001/js/main.js
@@ -2,11 +2,22 @@ const url = "https://desafio.xlow.com.br/search";
 const productContainer = document.querySelector("#products-container");
 const productCountInput = document.querySelector("#product-count-input");
 
+async function getProductsById(id) {
+  const urlUniqueProduct = `https://desafio.xlow.com.br/search/${id}`;
+  try {
+    const response = await fetch(urlUniqueProduct);
+    const data = await response.json();
+    return data
+  } catch (error) {
+    console.error("Erro ao obter produtos:", error);
+  }
+
+}
+
 async function getAllProducts() {
   try {
     const response = await fetch(url);
     const data = await response.json();
-
     const countH1 = document.createElement("h1");
     countH1.classList.add("product-modal");
     countH1.innerText = `Total Products: ${data.length}`;
@@ -17,6 +28,7 @@ async function getAllProducts() {
     const productCount = parseInt(productCountInput.value) || data.length;
 
     for (let i = 0; i < productCount && i < data.length; i++) {
+      const prodImagens = await getProductsById(data[i].productId)
       const prod = data[i];
 
       const card = document.createElement("li");
@@ -29,8 +41,11 @@ async function getAllProducts() {
       brand.innerText = `Marca: ${prod.brand}`;
 
       const image = document.createElement("img");
+      image.classList.add('product-image')
       image.src = prod.image;
       image.alt = prod.productName;
+
+
 
       const listPrice = document.createElement("p");
       listPrice.innerText = `Preço de Lista: R$ ${prod.listPrice / 100}`;
@@ -38,6 +53,20 @@ async function getAllProducts() {
 
       const bestPrice = document.createElement("p");
       bestPrice.innerText = `Melhor Preço: R$ ${prod.bestPrice / 100}`;
+
+      const containerImg = document.createElement("div");
+      containerImg.classList.add('container-imgs')
+      for (let index = 0; index < prodImagens.items.length; index++) {
+        const image1 = document.createElement("img");
+        image1.classList.add('small-image')
+        image1.src = prodImagens.items[index].images[0].imageUrl;
+        image1.alt = prodImagens.items[index].images[0].imageText;
+        image1.addEventListener('click', function () {
+          image.src = image1.src;
+          image.alt = image1.alt;
+        });
+        containerImg.appendChild(image1)
+      }
 
       const button = document.createElement("button");
       button.innerText = "Ver Produto";
@@ -47,6 +76,7 @@ async function getAllProducts() {
       card.appendChild(image);
       card.appendChild(listPrice);
       card.appendChild(bestPrice);
+      card.appendChild(containerImg)
       card.appendChild(button)
       productContainer.appendChild(card);
     }
